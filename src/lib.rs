54f320183f4084c6df75b9d19b507ce412380c22
bgok <ken@citizenhodler.com>
2021-05-04 10:20:48 -0700

    Extract and fix query tokens test

--- src/lib.rs
+++ src/lib.rs
@@ -33,6 +33,7 @@ mod unittest_query_minters;
 mod unittest_query_all_tokens;
 mod unittest_query_nft_dossier;
 mod unittest_query_num_tokens;
+mod unittest_query_tokens;
 mod unittest_register_receive_nft;
 mod unittest_send;
 mod unittest_set_global_approval;
